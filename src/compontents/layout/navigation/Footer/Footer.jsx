3ae8ca2bafd16e1e23d7d80b41e33b0bb6aa389a
damnnou <skdamn@outlook.com>
2023-10-05 23:40:13 +0300

    footer done

--- src/compontents/layout/navigation/Footer/Footer.jsx
+++ src/compontents/layout/navigation/Footer/Footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './footer.scss'
+import { FaCcVisa, FaCcMastercard, FaCcPaypal, FaCcStripe, FaCcApplePay, FaGooglePay } from 'react-icons/fa'
 
 const Footer = () => {
     return (
-    <footer>
-        <p>hi</p>
+    <footer className='main-footer'>
+        <div className='main-footer-copyrights'>
+            <p>© 2023 ALL RIGHTS RESERVED | BY SUNCOFFEE.COM</p>
+        </div>
+        <div className='main-footer-payments'>
+            <ul className='main-footer-payments-list'>
+                <li><FaCcVisa /></li>
+                <li><FaCcMastercard /></li>
+                <li><FaCcPaypal /></li>
+                <li><FaCcStripe /></li>
+                <li><FaCcApplePay /></li>
+                <li><FaGooglePay /></li>
+            </ul>
+        </div>
     </footer>
     )
 }
