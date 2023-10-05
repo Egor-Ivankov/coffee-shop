164bbed19ddcafbc4ad831ddb3538b24bbceb970
Egor-Ivankov <egor.ivankov2015@bk.ru>
2023-10-05 18:45:41 +0300

    add navBar

---
+++ src/compontents/layout/navigation/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from '../../../../assets/logo/logo.png';
+import shoppingCartIcon from '../../../../assets/icons/shopping-cart-2.png';
+import './navBar.scss';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div className='top-nav-bar'>
+                <img className='top-nav-bar-logo' src={logo} alt="logo"/>
+                <ul className='top-nav-bar-list'>
+                    <li className='top-nav-bar-list-item'>Home</li>
+                    <li className='top-nav-bar-list-item'>About us</li>
+                    <li className='top-nav-bar-list-item'>Products</li>
+                    <li className='top-nav-bar-list-item'>Become a partner</li>
+                    <li className='top-nav-bar-list-item'>Enquiry</li>
+                    <li className='top-nav-bar-list-item'>Login</li>
+                    <li className='top-nav-bar-list-item'>
+                        <img 
+                        className='top-nav-bar-list-item-icon'
+                        src={shoppingCartIcon} alt="shopping-cart-icon" 
+                        />
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './compontents//styles/index.scss';
+import NavBar from './compontents/layout/navigation/navBar/navBar';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>hi</h1>
+    <NavBar/>
   </React.StrictMode>,
 )
