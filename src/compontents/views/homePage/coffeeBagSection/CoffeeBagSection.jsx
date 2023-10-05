27b6f8aece867840438b648bc1c41dcffd091bbb
Egor-Ivankov <egor.ivankov2015@bk.ru>
2023-10-05 23:30:37 +0300

    add CoffeeBagSection

--- src/compontents/views/homePage/coffeeBagSection/CoffeeBagSection.jsx
+++ src/compontents/views/homePage/coffeeBagSection/CoffeeBagSection.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import coffeeBag from '../../../../assets/img/coffee-bag.png';
+import lines from '../../../../assets/img/lines.png';
+import coffeeKernels from '../../../../assets/img/coffee-kernels.png'
 import './coffeeBagSection.scss';
 
 const CoffeeBagSection = () => {
     return (
-        <section>
-            <div className='coffee-bag'>
-                <p className='coffee-bag-par'>Introducing the <br/> innovate yet <br/> simple</p>
+        <section className='coffee-bag'>
+                <div className='coffee-bag-introducing'>
+                    <p className='coffee-bag-introducing-par'>Introducing the <br/> innovate yet <br/> simple</p>
+                    <img className='coffee-bag-introducing-lines' src={lines} alt="lines" />
+                </div>
                 <img className='coffee-bag-image' src={coffeeBag} alt="coffee-bag" />
-                <p className='coffee-bag-par'>Stepped <br/> brewing <br/> method</p>
-            </div>
+                <div className='coffee-bag-stepped'>
+                    <p className='coffee-bag-stepped-par'>Steeped <br/> brewing <br/> method</p>
+                    <img className='coffee-bag-stepped-kernels' src={coffeeKernels} alt="coffee-kernels"/>
+                </div>
+
         </section>
     );
 }
