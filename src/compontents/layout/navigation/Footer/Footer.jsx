430e7f680a4f11cddd81c1304ec4b8aa5f406035
damnnou <skdamn@outlook.com>
2023-10-06 00:25:08 +0300

    fixed footer

--- src/compontents/layout/navigation/Footer/Footer.jsx
+++ src/compontents/layout/navigation/Footer/Footer.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-<<<<<<< HEAD
 import './footer.scss';
 import { FaCcVisa, FaCcMastercard, FaCcPaypal, FaCcStripe, FaCcApplePay, FaGooglePay } from 'react-icons/fa';
-=======
-import './footer.scss'
-import { FaCcVisa, FaCcMastercard, FaCcPaypal, FaCcStripe, FaCcApplePay, FaGooglePay } from 'react-icons/fa'
->>>>>>> 3ae8ca2bafd16e1e23d7d80b41e33b0bb6aa389a
 
 const Footer = () => {
     return (
     <footer className='main-footer'>
         <div className='main-footer-copyrights'>
-<<<<<<< HEAD
             <p className='main-footer-copyrights-p'>© 2023 ALL RIGHTS RESERVED | BY SUNCOFFEE.COM</p>
         </div>
         <div className='main-footer-payments'>
@@ -22,18 +16,6 @@ const Footer = () => {
                 <li className='main-footer-payments-list-item'><FaCcStripe /></li>
                 <li className='main-footer-payments-list-item'><FaCcApplePay /></li>
                 <li className='main-footer-payments-list-item'><FaGooglePay /></li>
-=======
-            <p>© 2023 ALL RIGHTS RESERVED | BY SUNCOFFEE.COM</p>
-        </div>
-        <div className='main-footer-payments'>
-            <ul className='main-footer-payments-list'>
-                <li><FaCcVisa /></li>
-                <li><FaCcMastercard /></li>
-                <li><FaCcPaypal /></li>
-                <li><FaCcStripe /></li>
-                <li><FaCcApplePay /></li>
-                <li><FaGooglePay /></li>
->>>>>>> 3ae8ca2bafd16e1e23d7d80b41e33b0bb6aa389a
             </ul>
         </div>
     </footer>
