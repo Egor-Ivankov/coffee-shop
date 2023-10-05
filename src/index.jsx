118d6de4acea40a8e5f0f00d3b9a739e95dca12d
Egor-Ivankov <122007814+Egor-Ivankov@users.noreply.github.com>
2023-10-05 17:03:40 +0300

    project init

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './compontents//styles/index.scss';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>hi</h1>
+  </React.StrictMode>,
+)
