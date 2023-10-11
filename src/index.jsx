bf6a48c1feb202a9886503afb69fe66251b39eca
damnnou <skdamn@outlook.com>
2023-10-11 18:27:20 +0300

    style newsletter-section

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom/client'
 import '@components/styles/index.scss';
 import { NavBar } from '@components/layout';
 import { Footer } from '@components/layout'
-import { CoffeeBagSection } from './views/homePage';
-import { FooterSection } from './views/homePage';
-import { BenefitsSection } from './views/homePage';
-import { CupCoffeeSection } from './views/homePage';
-import { VideoSection } from './views/homePage';
-import { HowToSleepSection } from './views/homePage';
+import { CoffeeBagSection, BenefitsSection, CupCoffeeSection, VideoSection, HowToSleepSection, NewsletterSection, FooterSection } from './views/homePage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -18,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <CupCoffeeSection />
     <VideoSection />
     <HowToSleepSection />
+    <NewsletterSection />
     <FooterSection />
     <Footer />
   </React.StrictMode>
