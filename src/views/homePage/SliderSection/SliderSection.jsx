9c99e46179070c7b91453bf7c834922e4ee03f5b
Egor Ivankov <egor.ivankov2015@bk.ru>
2023-10-13 22:03:25 +0300

    SliderSection styling is complete

--- src/views/homePage/SliderSection/SliderSection.jsx
+++ src/views/homePage/SliderSection/SliderSection.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import coffeeBag from '@assets/img/coffee-bag.png';
+import lines from '@assets/img/lines.png'
+import {BsArrowRightShort} from 'react-icons/bs';
+import { BsArrowLeftShort } from 'react-icons/bs';
 import './sliderSection.scss';
 
 const SliderSection = () => {
     return (
         <section className='slider-section'>
-            <div className='slider-section-background'></div>
-            <img src={coffeeBag} alt="coffee-bag" />
-            <img src={coffeeBag} alt="coffee-bag" />
-            <img src={coffeeBag} alt="coffee-bag" />
-            <div className='slider-section-buttons'>
-                <button className='slider-section-buttons-left'>left</button>
-                <button className='slider-section-buttons-right'>right</button>
+            <div className='slider-section-information'>
+                <h2 className='slider-section-information-header'>Our products</h2>
+                <p className='slider-section-information-description'>
+                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit <br/> 
+                    aut fugit, sed quia consequuntur magni dolores eos
+                </p>
+            </div>
+            <img className='slider-section-lines' src={lines} alt="lines" />
+            <img className='slider-section-coffee' src={coffeeBag} alt="coffee-bag" />
+            <img className='slider-section-coffee active' src={coffeeBag} alt="coffee-bag" />
+            <img className='slider-section-coffee' src={coffeeBag} alt="coffee-bag" />
+            <div className='slider-section-container'>
+                <p className='slider-section-container-name'>Sun coffee</p>
+                <p className='slider-section-container-price'>100$</p>
+                <div className='slider-section-container-buttons'>
+                    <BsArrowLeftShort className='slider-section-container-buttons-item'/>
+                    <BsArrowRightShort className='slider-section-container-buttons-item'/>
+                </div>
             </div>
         </section>
     )
