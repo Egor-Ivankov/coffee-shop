2d8c1e6a07288229f15598d04076b76f50ba4cd9
damnnou <skdamn@outlook.com>
2023-10-13 22:45:49 +0300

    add routes

--- src/components/layout/navigation/NavBar/NavBar.jsx
+++ src/components/layout/navigation/NavBar/NavBar.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import logo from '@assets/logo/logo.png';
 import { FiShoppingCart } from 'react-icons/fi';
 import './navBar.scss';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <nav className='top-nav-bar'>
                 <img className='top-nav-bar-logo' src={logo} alt="logo"/>
                 <ul className='top-nav-bar-list'>
-                    <li className='top-nav-bar-list-item'>Home</li>
-                    <li className='top-nav-bar-list-item'>About us</li>
-                    <li className='top-nav-bar-list-item'>Products</li>
-                    <li className='top-nav-bar-list-item'>Become a partner</li>
-                    <li className='top-nav-bar-list-item'>Enquiry</li>
-                    <li className='top-nav-bar-list-item'>Login</li>
+                    <li className='top-nav-bar-list-item'><Link to="/">Home</Link></li>
+                    <li className='top-nav-bar-list-item'><Link to="/aboutUs">About us</Link></li>
+                    <li className='top-nav-bar-list-item'><Link to="/products">Products</Link></li>
+                    <li className='top-nav-bar-list-item'><Link to="/becomeAPartner">Become a partner</Link></li>
+                    <li className='top-nav-bar-list-item'><Link to="/enquiry">Enquiry</Link></li>
+                    <li className='top-nav-bar-list-item'><Link to="/login">Login</Link></li>
                     <li className='top-nav-bar-list-item'>
                         <FiShoppingCart className='top-nav-bar-list-item-icon' />
                     </li>
