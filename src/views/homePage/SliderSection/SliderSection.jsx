17fef0a53fa41fce14091af894bb1d46a82db484
Egor Ivankov <egor.ivankov2015@bk.ru>
2023-10-14 20:48:11 +0300

    Working on the slider

--- src/views/homePage/SliderSection/SliderSection.jsx
+++ src/views/homePage/SliderSection/SliderSection.jsx
@@ -1,13 +1,68 @@
-import React from 'react';
-import coffeeBag from '@assets/img/coffee-bag.png';
+import React, { useState, useEffect } from 'react';
 import lines from '@assets/img/lines.png'
-import {BsArrowRightShort} from 'react-icons/bs';
+import coffeeBag from '@assets/img/coffee-bag.png';
+import { BsArrowRightShort } from 'react-icons/bs';
 import { BsArrowLeftShort } from 'react-icons/bs';
+import { nanoid } from 'nanoid';
 import './sliderSection.scss';
 
 const SliderSection = () => {
+    const [items, setItems] = useState([]);
+    const [slide, setSlide] = useState(0);
+    
+    useEffect(() => {
+        setItems([
+            {   
+                image: coffeeBag, 
+                title: 'Sun coffee 1', 
+                price: 100, 
+                id: nanoid()
+            },
+            {
+                image: coffeeBag, 
+                title: 'Sun coffee 2', 
+                price: 200, 
+                id: nanoid()
+            },
+            {
+                image: coffeeBag, 
+                title: 'Sun coffee 3', 
+                price: 300, 
+                id: nanoid()
+            },
+        ]);
+    }, []);
+
+    const renderItems = (arr) => {
+        return arr.map((item, index) => {
+            const {image, title, id} = item;
+
+            let position = ' nextSlide';
+
+            if (slide === index) {
+                position = ' activeSlide';
+            }
+
+            if (slide === index - 1 || (index === 0 && slide === items.length - 1)) {
+                position = ' lastSlide'
+            }
+
+            return  (
+                <img 
+                    className={`slider-section-coffee ${position}`} 
+                    src={image} 
+                    alt={title} 
+                    key={id}
+                />
+            );
+        });
+    }
+
+    const elements = renderItems(items);
+
     return (
         <section className='slider-section'>
+
             <div className='slider-section-information'>
                 <h2 className='slider-section-information-header'>Our products</h2>
                 <p className='slider-section-information-description'>
@@ -15,18 +70,26 @@ const SliderSection = () => {
                     aut fugit, sed quia consequuntur magni dolores eos
                 </p>
             </div>
+
             <img className='slider-section-lines' src={lines} alt="lines" />
-            <img className='slider-section-coffee' src={coffeeBag} alt="coffee-bag" />
-            <img className='slider-section-coffee active' src={coffeeBag} alt="coffee-bag" />
-            <img className='slider-section-coffee' src={coffeeBag} alt="coffee-bag" />
+
+            {elements}
+
             <div className='slider-section-container'>
                 <p className='slider-section-container-name'>Sun coffee</p>
                 <p className='slider-section-container-price'>100$</p>
                 <div className='slider-section-container-buttons'>
-                    <BsArrowLeftShort className='slider-section-container-buttons-item'/>
-                    <BsArrowRightShort className='slider-section-container-buttons-item'/>
+                    <BsArrowLeftShort 
+                        onClick={() => setItems(slide => slide - 1)}
+                        className='slider-section-container-buttons-item'
+                    />
+                    <BsArrowRightShort 
+                        onClick={() => setSlide(slide => slide + 1)}
+                        className='slider-section-container-buttons-item'
+                    />
                 </div>
             </div>
+
         </section>
     )
 }
