d1da00a35951b5827393c37d620a5a2fcc182ab0
damnnou <skdamn@outlook.com>
2023-10-15 22:29:30 +0300

    change api url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:3002/api"
+    baseURL: "https://coffee-shop-api-9alx.onrender.com/api"
 });
 
 instance.interceptors.request.use(config => {
