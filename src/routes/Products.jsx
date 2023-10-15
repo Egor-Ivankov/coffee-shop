54cbf87e62bdf2ef0beaa0f67476acdfcd8f4ec1
damnnou <skdamn@outlook.com>
2023-10-15 19:44:03 +0300

    get products from mongoDB

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import axios from '../utils/axios';
+import React, {useEffect, useState} from 'react'
+import { ProductsSection, ProductsItem } from '../views/productsPage';
+import { Link } from 'react-router-dom';
 
 const Products = () => {
+
+  const [products, setProducts] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get('/products');
+      const filteredData = data.products;
+      setProducts(filteredData)
+      console.log('fetched')
+    } catch (error) {
+      console.error('Ошибка при получении данных:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  },[])
+
   return (
-    <div>Products</div>
+    <div className='products-page'>
+      <h1>Developing...</h1>
+      <Link to='/' ><h3>back to home</h3></Link>
+      <ProductsSection products={products}  />
+      
+    </div>
   )
 }
 
---
+++ src/views/productsPage/productsSection/ProductsSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ProductsItem } from '../'
+import './productsSection.scss'
+
+const ProductsSection = ({products}) => {
+  return (
+    <div className='products-section'>
+        {products.map(item => (
+            <ProductsItem
+            key={item._id}
+            title={item.title}
+            img={item.img}
+            price={item.price}
+            description={item.description}
+            />
+        ))}
+    </div>
+  )
+}
+
+export default ProductsSection
