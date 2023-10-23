1880ff3c99d58b42f015b401ec5b34fa54d02b6b
Egor Ivankov <egor.ivankov2015@bk.ru>
2023-10-23 20:49:48 +0300

    add region and strength filter

--- src/views/productsPage/productsSection/ProductsSection.jsx
+++ src/views/productsPage/productsSection/ProductsSection.jsx
@@ -1,25 +1,56 @@
-import React from 'react'
-import { ProductsItem } from '..'
-import './productsSection.scss'
+import React from 'react';
+import { ProductsItem } from '..';
+import './productsSection.scss';
+import { useSelector } from 'react-redux';
 
 const ProductsSection = ({products}) => {
-  return (
-    <div className='products-section'>
-        {products.map(item => (
-            <ProductsItem
-            key={item._id}
-            title={item.title}
-            img={item.img}
-            price={item.price}
-            description={item.description}
-            flavours={item.flavours}
-            region={item.region}
-            environment={item.environment}
-            strength={item.strength}
-            />
-        ))}
-    </div>
-  )
+
+    const regions = useSelector(state => state.productsFilter.regions)
+    const flavour = useSelector(state => state.productsFilter.flavour)
+    const strength = useSelector(state => state.productsFilter.strength)
+
+
+    const filteredRegion = (products, filter) => {
+        if (filter === 'all') {
+        return products;
+        } else {
+        return products.filter(product => product.region.name.toLowerCase() === filter);
+        }
+    }
+
+    const filteredStrength = (products, filter) => {
+        if (filter === 'all') {
+        return products;
+        } else {
+        return products.filter(product => product.strength == filter);
+        }
+    }
+
+    const renderFilteredProducts = () => {
+        const filteredRegionProducts = filteredRegion(products, regions);
+        const filteredStrengthProducts = filteredStrength(filteredRegionProducts, strength);
+        return filteredStrengthProducts.map(product => {
+            return <ProductsItem
+                        key={product._id}
+                        title={product.title}
+                        img={product.img}
+                        price={product.price}
+                        description={product.description}
+                        flavours={product.flavours}
+                        region={product.region}
+                        environment={product.environment}
+                        strength={product.strength}
+                    />
+        })
+    }
+
+    const allProducts = renderFilteredProducts();
+
+    return (
+        <div className='products-section'>
+            {allProducts}
+        </div>
+    )
 }
 
 export default ProductsSection
