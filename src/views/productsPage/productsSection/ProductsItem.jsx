94882617c72b93162638a4c74f397bcecec708f5
damnnou <skdamn@outlook.com>
2023-10-24 17:30:25 +0300

    fix flavours in productsItem

--- src/views/productsPage/productsSection/ProductsItem.jsx
+++ src/views/productsPage/productsSection/ProductsItem.jsx
@@ -1,16 +1,25 @@
 import React, { useMemo } from 'react'
 import './productsItem.scss'
-import parse from 'html-react-parser';
+import strength5 from '@assets/img/strength5.png'
+import strength4 from '@assets/img/strength4.png'
+import strength3 from '@assets/img/strength3.png'
 
 const ProductsItem = ({img, price, description, title, flavours, strength}) => {
 
-  console.log(flavours.length)
+  const renderFlavours = flavours.map(item => {
+    if (item !== flavours[flavours.length - 1]) {
+      return <span> {item.name + ' • '} </span>
+    } else {
+      return <span>{item.name}</span>
+    }
+  }
+  )
 
   return (
     <div className='products-item'>
         <img className='products-item-img' src={img} />
         <h3 className='products-item-title'>{title.toString()}</h3>
-        <p className='products-item-flavours'>{flavours.map(item => <span> {item.name + ' • '} </span>)}</p>
+        <p className='products-item-flavours'>{renderFlavours}</p>
         <p className='products-item-price'>From $ {price}</p>
         <button className='products-item-button'>Quick view</button>
         <div className='products-item-strength'>
