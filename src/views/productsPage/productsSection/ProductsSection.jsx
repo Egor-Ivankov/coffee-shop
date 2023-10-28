0164121939f3e7c535873d29e6e90f32f2df95b2
damnnou <skdamn@outlook.com>
2023-10-28 19:40:21 +0300

    fix filter + add flavours filter

--- src/views/productsPage/productsSection/ProductsSection.jsx
+++ src/views/productsPage/productsSection/ProductsSection.jsx
@@ -10,35 +10,99 @@ const ProductsSection = ({products}) => {
     const flavour = useSelector(state => state.productsFilter.flavour);
     const strength = useSelector(state => state.productsFilter.strength);
 
-
     const filteredRegion = (products, filter) => {
-        if (filter === 'all') {
-        return products;
-        } else {
-        return products.filter(product => product.region.name.toLowerCase() === filter);
-        }
+        return (
+            filter === 'all'
+            ?
+            products
+            :
+            products.filter(product => product.region.name.toLowerCase() === filter)
+        )
+    }
+
+    const filteredFlavour = (products, filter) => {
+        return (
+            filter === 'all'
+            ?
+            products
+            :
+            products.filter(product => {
+                const flavourName = product.flavours.map(flavour => flavour.name.toLowerCase());
+                switch (filter) {
+                    case 'chocolate':
+                        return flavourName.some(name => 
+                            name.includes('chocolate') 
+                            || 
+                            name.includes('caramel') 
+                            || 
+                            name.includes('butter')
+                            ||
+                            name.includes('toffee')
+                        );
+                    case 'citrus':
+                        return flavourName.some(name => 
+                            name.includes('citrus') 
+                            || 
+                            name.includes('boiled sweets')
+                            ||
+                            name.includes('marmalade')
+                        );
+                    case 'fruit':
+                        return flavourName.some(name => 
+                            name.includes('berry') 
+                            || 
+                            name.includes('grape')
+                            ||
+                            name.includes('fruit')
+                            ||
+                            name.includes('cherry')
+                        );
+                    case 'nut':
+                        return flavourName.some(name => 
+                            name.includes('nut') 
+                            || 
+                            name.includes('almond')
+                            ||
+                            name.includes('spice')
+                        );
+                    case 'strong':
+                        return flavourName.some(name => 
+                            name.includes('dark')
+                            ||
+                            name.includes('tobacco')
+                            ||
+                            name.includes('cloves')
+                            )
+                }
+            })
+        )
     }
 
     const filteredStrength = (products, filter) => {
-        if (filter === 'all') {
-        return products;
-        } else {
-        return products.filter(product => product.strength == filter);
-        }
+        return (
+            filter === 'all'
+            ?
+            products
+            :
+            products.filter(product => product.strength == filter)
+        ) 
     }
 
     const renderFilteredProducts = (products) => {
-        const filteredRegionProducts = filteredRegion(products, regions);
-        const filteredStrengthProducts = filteredStrength(filteredRegionProducts, strength);
+        let filteredProducts = [...products];
 
-        if (filteredStrengthProducts.length === 0) {
+        filteredProducts = filteredRegion(filteredProducts, regions);
+        filteredProducts = filteredFlavour(filteredProducts, flavour)
+        filteredProducts = filteredStrength(filteredProducts, strength);
+
+        if (filteredProducts.length === 0) {
             return (
                 <CSSTransition timeout={400} classNames='product'>
                     <div className='empty'>Coffee list is empty</div>
                 </CSSTransition>
             )
         } else {
-            return filteredStrengthProducts.map(product => {
+            return filteredProducts.map(product => {
                 return (
                     <CSSTransition timeout={400} classNames="product" key={product._id}>
                         <ProductsItem
