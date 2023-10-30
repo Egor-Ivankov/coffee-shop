01bf4a64bc4a3caf163a4639ef3a43c0d30e021c
damnnou <skdamn@outlook.com>
2023-10-30 18:30:40 +0300

    style BagSize section in modalWindow

--- src/views/productsPage/ModalWindowItem/ModalWindowItem.jsx
+++ src/views/productsPage/ModalWindowItem/ModalWindowItem.jsx
@@ -6,7 +6,7 @@ import strength3 from '@assets/img/strength3.png';
 import parse from 'html-react-parser';
 import { AiOutlineClose } from 'react-icons/ai';
 
-const ModalWindowItem = ({img, price, description, title, flavours, strength, setModal, modal}) => {
+const ModalWindowItem = ({id, img, price, description, title, strength, setModal}) => {
 
   const renderImg = 
   strength === 3 
@@ -21,13 +21,20 @@ const ModalWindowItem = ({img, price, description, title, flavours, strength, se
   ?? 
   strength5;
 
+  /* Order Variables */
+  const [bagSize, setBagSize] = useState(0.2);
+  const [quantity, setQuantity] = useState(1);
+  const [grind, setGrind] = useState('Wholebean');
+  const sum = price * (bagSize + 0.8);
+
+  /* Close Animation functionality */
   const [closing, setClosing] = useState(false);
 
   useEffect(() => {
     if (closing) {
       const timer = setTimeout(() => {
         setModal(false);
-      }, 400); // Задержка должна соответствовать времени анимации
+      }, 400);
       return () => clearTimeout(timer);
     }
   }, [closing, setModal]);
@@ -36,10 +43,17 @@ const ModalWindowItem = ({img, price, description, title, flavours, strength, se
     setClosing(true);
   };
 
+  /* Add to Cart functionality */
+  const handleAddToCart = (props) => {
+    // post to db with props - bag Size, quantity, grind, price, product id
+    // set button AddToCart into Checkout
+    console.log(props);
+  }
+
   return (
     <div onClick={(e) => e.target.id === 'modalWindow' && closeModal()} id='modalWindow' className={`modal-window ${closing ? 'closed' : ''}`} >
         <div className='modal-window-container'>
-            <AiOutlineClose className='modal-window-container-close-button' onClick={() => setModal(false)} />
+            <AiOutlineClose className='modal-window-container-close-button' onClick={() => closeModal()} />
             <h1 className='modal-window-container-title'>{title}</h1>
             <div className='modal-window-container-inside'>
                 <div className='modal-window-container-inside-information'>
@@ -52,31 +66,33 @@ const ModalWindowItem = ({img, price, description, title, flavours, strength, se
                 <form onSubmit={e => e.preventDefault()} className='modal-window-container-inside-form'>
                     <label className='modal-window-container-inside-form-label'>
                         <h2 className='modal-window-container-inside-form-label-title'>Quantity</h2>
-                        <input className='modal-window-container-inside-form-label-input' type='number' />
+                        <input value={quantity} onChange={(e) => setQuantity(e.target.value)} className='modal-window-container-inside-form-label-input' type='number' />
                     </label>
 
                     <div className='modal-window-container-inside-form-label'>
                         <h2 className='modal-window-container-inside-form-label-title'>Bag Size</h2>
                         <div className='modal-window-container-inside-form-label-circles'>
-                            <button className='modal-window-container-inside-form-label-circles-option'>200g</button>
-                            <button className='modal-window-container-inside-form-label-circles-option'>500g</button>
-                            <button className='modal-window-container-inside-form-label-circles-option'>1kg</button>
+                            <button onClick={(e) => setBagSize(0.2)} className={`modal-window-container-inside-form-label-circles-option ${bagSize === 0.2 ? ' active' : ''}`}>200g</button>
+                            <button onClick={(e) => setBagSize(0.5)} className={`modal-window-container-inside-form-label-circles-option ${bagSize === 0.5 ? ' active' : ''}`}>500g</button>
+                            <button onClick={(e) => setBagSize(1)} className={`modal-window-container-inside-form-label-circles-option ${bagSize === 1 ? ' active' : ''}`}>1kg</button>
                         </div>
                     </div>
 
                     <label className='modal-window-container-inside-form-label'>
                         <h2 className='modal-window-container-inside-form-label-title'>Please choose your grind</h2>
-                        <select className='modal-window-container-inside-form-label-select'>
-                            <option className='modal-window-container-inside-form-label-select-option'>Wholebean</option>
-                            <option className='modal-window-container-inside-form-label-select-option'>Cafetiere</option>
-                            <option className='modal-window-container-inside-form-label-select-option'>Filter</option>
-                            <option className='modal-window-container-inside-form-label-select-option'>Aeropress</option>
-                            <option className='modal-window-container-inside-form-label-select-option'>Stovetop</option>
-                            <option className='modal-window-container-inside-form-label-select-option'>Espresso</option>
+                        <select onChange={e => setGrind(e.target.value)} className='modal-window-container-inside-form-label-select'>
+                            <option className='modal-window-container-inside-form-label-select-option' value={'Wholebean'}>Wholebean</option>
+                            <option className='modal-window-container-inside-form-label-select-option' value={'Cafetiere'}>Cafetiere</option>
+                            <option className='modal-window-container-inside-form-label-select-option' value={'Filter'}>Filter</option>
+                            <option className='modal-window-container-inside-form-label-select-option' value={'Aeropress'}>Aeropress</option>
+                            <option className='modal-window-container-inside-form-label-select-option' value={'Stovetop'}>Stovetop</option>
+                            <option className='modal-window-container-inside-form-label-select-option' value={'Espresso'}>Espresso</option>
                         </select>
                     </label>
 
-                    <button className='modal-window-container-inside-form-label-button'>Add to cart - $ {price}</button>
+                    <button 
+                    onClick={() => handleAddToCart({sum, bagSize, grind})}
+                    className='modal-window-container-inside-form-label-button'>Add to cart - ${sum}</button>
                 </form>
             </div>
         </div>
